5bbb4e970a7f74872de9deb2dbf661603db85595
danielsogitec <127794363+danielsogitec@users.noreply.github.com>
2023-11-16 23:29:55 -0300

    vamos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export * from './lib/sdk';
+/* eslint-disable lines-around-comment */
+/* eslint-disable @nx/enforce-module-boundaries */
+// export * from '../../libs/shared/src/lib/domain/value-objects/valueobject';
+// export * from '../../libs/shared/src/lib/result/result';
+// export * from '../../libs/shared/src/lib/errors/app.error';
+// export * from '../../libs/shared/src/lib/errors/security.error';
+// export * from '../../libs/shared/src/lib/errors/domain.error';
+// export * from '../../libs/shared/src/lib/errors/use-case.error';
+// export * from '../../libs/shared/src/lib/errors/service.error';
+// export * from '../../libs/shared/src/lib/errors/external-service.error';
+// export * from '../../libs/shared/src/lib/errors/search.error';
+// export * from '../../libs/shared/src/lib/errors/text-search.error';
+// export * from '../../libs/shared/src/lib/types/null.type';
+// export * from '../../libs/shared/src/lib/types/id.type';
+// export * from '../../libs/shared/src/lib/types/context.type';
+// export * from '../../libs/shared/src/lib/types/search.type';
+// export * from '../../libs/shared/src/lib/types/text-search.type';
+// export * from '../../libs/shared/src/lib/types/findby.type';
+// export * from '../../libs/shared/src/lib/utils/string.utils';
+export * from '../../libs/common/pessoas/src/lib/domain/value-objects';
+export * from '../../libs/common/pessoas/src/lib/types/cnpj.type';
+// export * from '../../libs/shared/src/lib/errors/controller.error';
+// export * from '../../libs/shared/src/lib/errors/dto.error';
+// export * from '../../libs/shared/src/lib/errors/repo.error';
+// export * from '../../libs/shared/src/lib/errors/repo-static.error';
