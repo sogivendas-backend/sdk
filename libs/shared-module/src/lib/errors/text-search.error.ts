a160a6a78b9df5e6543adc759764106465e93cdd
danielsogitec <127794363+danielsogitec@users.noreply.github.com>
2023-11-17 13:53:16 -0300

    restruct

--- libs/shared-module/src/lib/errors/text-search.error.ts
+++
@@ -1,1 +0,0 @@
-export * from "../../../../../../shared/src/lib/errors/text-search.error"
--- libs/shared-module/src/lib/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './null.type';
-export * from './id.type';
-export * from './context.type';
-export * from './search.type';
-export * from './text-search.type';
-export * from './findby.type';
