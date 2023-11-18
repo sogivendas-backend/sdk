d7e530db509a416ce8023ba385f00ce43303d291
danielsogitec <127794363+danielsogitec@users.noreply.github.com>
2023-11-18 00:02:11 -0300

    vai

--- packages/common-module/src/pessoas/lib/domain/value-objects/cpf-cnpj-documento.ts
+++ packages/common-module/src/pessoas/lib/domain/value-objects/cpf-cnpj-documento.ts
@@ -1,7 +1,1 @@
-/**
- * Interface que declara os atributos em comuns dos documentos CPF e CNPJ
- */
-export interface CPFCNPJDocumento {
-	get desformatado(): string;
-	get formatado(): string;
-}
+export * from "../../../../../../../../common/pessoas/src/lib/domain/value-objects/cpf-cnpj-documento"
--- packages/common-module/src/pessoas/lib/types/cnpj.type.ts
+++ packages/common-module/src/pessoas/lib/types/cnpj.type.ts
@@ -1,5 +1,1 @@
-import { CNPJValueObject } from '../domain';
-
-export type CNPJFormatado = string;
-export type CNPJDesformatado = string;
-export type CNPJ<T> = T extends CNPJValueObject | CNPJFormatado | CNPJDesformatado | string ? T : never;
+export * from "../../../../../../../common/pessoas/src/lib/types/cnpj.type"
